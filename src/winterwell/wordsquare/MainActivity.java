ce84fa12fefa9b76258e2e0e3f5e6048eb25e133
daniel <daniel@winterwell.com>
2010-12-25 17:55:14 +0000

    first-pass

--- src/winterwell/wordsquare/MainActivity.java
+++ src/winterwell/wordsquare/MainActivity.java
@@ -23,9 +23,10 @@ public class MainActivity extends Activity {
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        webview = (WebView) findViewById(R.id.webView);
+        super.onCreate(savedInstanceState);        
+        webview = new WebView(this);
+        
+        setContentView(webview);
         //countdown = (TextView) findViewById(R.id.countdown);
 //        timerBar = (ProgressBar) findViewById(R.id.timerBar);
 //        setContentView(R.layout.main);
